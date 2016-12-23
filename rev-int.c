094385a923bac6ac3fc3d7ea9d3e0c956a161e18
Gokulkm01 <tamilangokul96@gmail.com>
2016-12-23 14:51:05 +0530

    Add files via upload

---
+++ rev-int.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a[10],*b;
+     printf("\n enter the values:");
+    scanf("\n %s",&a);
+    b=strrev(a);
+    printf("\n the values are:%s",b);
+
+}
