10b66922bf19593d1d1e48e73c3a3601debbf088
Gokulkm01 <tamilangokul96@gmail.com>
2016-12-28 10:12:23 +0530

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+    char ch[30];
+    printf("\n enter the char");
+    scanf("%s",ch);
+    if(ch[0]>'a'&&ch[0]<'z')
+    {
+
+
+    printf("\n it is alpha");
+    }
+    else{printf("\n its is not alpha");
+    }
+
+
+
+}
