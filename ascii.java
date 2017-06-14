9223d71ecf8c6855d576315cc75917d7d585d063
Gokulkm01 <tamilangokul96@gmail.com>
2017-06-14 09:49:31 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,12 @@
+package eg;
+
+import java.util.Scanner;
+
+public class ascii {
+	public static void main(String args[]){
+	for(int i=0;i<256;i++){
+	System.out.println(i +" : "+ (char)i);
+	}
+	}
+
+}
