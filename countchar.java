861be79d1bc8e1d5dd6b4fa70854e32950b0c348
Gokulkm01 <tamilangokul96@gmail.com>
2017-06-28 09:34:07 +0530

    Add files via upload

---
+++ countchar.java
@@ -0,0 +1,12 @@
+package eg;
+
+import java.util.Scanner;
+
+public class countchar {
+public static void main(String args[]){
+	String name;
+	Scanner scan=new Scanner(System.in);
+     name=scan.nextLine();
+		System.out.println("Number of character is "+name.length());
+}
+}
