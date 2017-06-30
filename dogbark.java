a3380844307d35a60682ebd10744f91bfa379fef
Gokulkm01 <tamilangokul96@gmail.com>
2017-06-30 16:02:14 +0530

    Add files via upload

---
+++ dogbark.java
@@ -0,0 +1,12 @@
+package guvi;
+
+public class dogbark {
+public static void main(String args[]){
+	String str = "" ;
+	String barksonce="smile";
+	String barkstwice="cry";
+	String barkstrice="fear";
+	
+	System.out.println("alex wake up with "+barkstrice+" and "+barkstwice+" before going school and return to home with "+barksonce);
+}
+}
