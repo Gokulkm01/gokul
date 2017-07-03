28d9b8c49a6ff8bf7a8bc7e78419d2c77c907ff6
Gokulkm01 <tamilangokul96@gmail.com>
2017-07-03 17:51:50 +0530

    Add files via upload

---
+++ wostr.java
@@ -0,0 +1,12 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class wostr {
+	public static void main (String args[]){
+		Scanner scan=new Scanner(System.in);
+		String str1=scan.next();
+		String str2=scan.next();
+		System.out.println(str1+str2);
+	}
+}
