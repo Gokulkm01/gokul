ca57c5cdffd0915c5575c3a8a3c03f97d4250955
Gokulkm01 <tamilangokul96@gmail.com>
2017-07-12 16:06:02 +0530

    Add files via upload

---
+++ trycoll.java
@@ -0,0 +1,8 @@
+package trycode;
+import java.util.*;
+public class trycoll {
+public static void main(String ards[]){
+	String str="22";
+	System.out.println(Character.getNumericValue(str.charAt(0))*Character.getNumericValue(str.charAt(1)));
+}
+}
