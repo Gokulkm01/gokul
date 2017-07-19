ce78105ba175be7fc589625b6a15dfdd4555236c
Gokulkm01 <tamilangokul96@gmail.com>
2017-07-19 10:23:50 +0530

    Add files via upload

--- hun5.java
+++ hun5.java
@@ -5,24 +5,14 @@ import java.util.stream.IntStream;
 
 public class hun5 {
 public static void main(String args[]){
-	String str="abc";
+	String str="12258";
 	String s=str.toLowerCase();
 	for(int i=0;i<s.length();i++){
-		int a=(int)s.charAt(i);
-		a=a-96;
-		String s1=Integer.toString(a);
-		System.out.print(a);
+		int a=s.charAt(i);
+		a=a+16;
+		char a1=(char)a;
+		String s1=Character.toString(a1);
+		System.out.print(s1.toLowerCase());
 	}
-/*	Scanner sc=new Scanner(System.in);
-    String a=sc.next();
-	String b=a.toLowerCase();
-	String s1="";
-	 for(int i=0;i<b.length();i++)
-	    {
-		    int a1=(int)b.charAt(i);
-		    a1=a1-96;
-		    String s=Integer.toString(a1);
-		    s1=s1+s;
-}System.out.println(s1);*/
 }
 }
