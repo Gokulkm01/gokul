1eeb28e6b49196a52c9c2a402aaa639e767929bd
Gokulkm01 <tamilangokul96@gmail.com>
2017-07-19 10:56:24 +0530

    Add files via upload

---
+++ hun3.java
@@ -0,0 +1,12 @@
+package guvi;
+
+public class hun3 {
+public static void main(String args[]){
+	int a[]=new int[]{-3,-1,1,3,5};
+	for(int i=0;i<a.length;i++){
+		if(a[i]==i){
+			System.out.println(a[i]+" "+i);
+	}
+}
+}
+}
