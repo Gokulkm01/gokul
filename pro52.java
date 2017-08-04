6384787157bb8b7c95fbd7d3481261ae4188072b
Gokulkm01 <tamilangokul96@gmail.com>
2017-08-04 11:47:05 +0530

    Add files via upload

---
+++ pro52.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pro52 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int c=scan.nextInt();
+int d=scan.nextInt();
+int e=scan.nextInt();
+int f=scan.nextInt();
+int g=scan.nextInt();
+int h=scan.nextInt();
+if(a==c&&e==g&&b==h&&d==f){
+	System.out.println("square");
+}
+else{
+	System.out.println("Not square");
+}
+	}
+
+}
